8a0b7ae276f6c73b4f6bfc7268eb4d1643acb9e2
bakeryenthusiast <claudioraciti01@gmail.com>
2023-03-22 17:14:25 +0100

    fitDistrAngolare esponente 2 accettato

--- fitDistrAngolare.C
+++ fitDistrAngolare.C
@@ -17,7 +17,8 @@ void fitDistrAngolare(){
 
 float angolo [] = {0.,15.,30.,45.,60.};
 float err_angolo[] = {1.,1.,1.,1.,1.};
-float err_rate[] = {0.0433 , 0.0421 , 0.0419 , 0.0396 , 0.0375};
+float err_rate[] = {0.0249,0.0227,0.0225,0.0177,0.0122};
+// float err_rate[] = {0.0325,0.0302,0.0298,0.0244,0.0171};
 float rate[] = {0.3673,0.3034,0.2275,0.1509,0.0418};
 
 //Grafico passa alto
@@ -32,13 +33,15 @@ gG1f->GetXaxis()->SetTitle("cos($\th$)");
 gG1f->GetYaxis()->SetTitle("rate");
 gG1f->Draw("AP");
 
-cout << "\n\n --- Ipotesi  [0] * pow(cos(x),[1]) --- \n" <<endl;
+cout << "\n\n --- Ipotesi  [0] * pow(cos(x*3.141592/180),[1])+[2] --- \n" <<endl;
 
-TF1 *funz1 = new TF1("funz1","[0] * pow(cos(x*3.141592/180),[1])",-0.1,250000);
+TF1 *funz1 = new TF1("funz1","[0] * pow(cos(x*3.141592/180),[1])+[2]",-0.1,250000);
 
 funz1->SetParameter(0,0.5);
+funz1->SetParameter(2,0.);
 funz1->SetParameter(1,2.);
-funz1->SetParLimits(0,0.,1.);
+// funz1->SetParLimits(0,0.,1.);
+funz1->SetParLimits(1,0.,4.);
 gG1f->Fit(funz1,"RM+");
 cout << "Chi^2:" << funz1->GetChisquare() << ", number of DoF: " << funz1->GetNDF() << " (Probability: " << funz1->GetProb() << ")." << endl;
 
