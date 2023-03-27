85dbb4486c0e0ebd87348801240767fd076485bc
ggiorggia <giorgia.vigliaturo@edu.unito.it>
2023-03-27 23:12:53 +0200

    Update fitAccDist_G.C

--- fitAccDist_G.C
+++ fitAccDist_G.C
@@ -40,6 +40,30 @@ gG1f->GetXaxis()->SetTitle("d");
 gG1f->GetYaxis()->SetTitle("rate");
 gG1f->Draw("AP");
 
+TF1 *funz1 = new TF1("funz1"," [0]*exp(-[1]*x)",-0.1,250000);
+
+funz1->SetParameter(0,3.);
+funz1->SetParameter(1,0.5);
+//funz1->SetParLimits(0,0.,190.);
+// funz1->SetParLimits(2,70.,76.);
+
+/*TF1 *funz2 = new TF1("funz2","[0]*x+[1]",-0.1,250000);
+
+funz2->SetParameter(0,-3.);
+funz2->SetParameter(1,3);
+//funz2->SetParLimits(0,0.,190.);
+// funz2->SetParLimits(2,70.,76.);*/
+
+
+gG1f->Fit(funz1,"RM+");
+cout << endl << "tried: [0]*exp(-[1]*x)" << endl;
+cout << "Chi^2:" << funz1->GetChisquare() << ", number of DoF: " << funz1->GetNDF() << " (Probability: " << funz1->GetProb() << ")." << endl;
+float W1= funz1->GetChisquare()/funz1->GetNDF();
+/*gG1f->Fit(funz2,"RM+");
+cout << endl << "tried: [0]*x+[1]" << endl;
+cout << "Chi^2:" << funz2->GetChisquare() << ", number of DoF: " << funz2->GetNDF() << " (Probability: " << funz2->GetProb() << ")." << endl;
+*/
+
 TCanvas *cG2f = new TCanvas("Accettanza","Accettanza normalizzata G(d)",400,10,600,400);
 cG2f->SetLogx(1);
 cG2f->SetFillColor(0);
@@ -51,5 +75,19 @@ gG2f->SetTitle("Accettanza normalizzata G(d)");
 gG2f->GetXaxis()->SetTitle("d");
 gG2f->GetYaxis()->SetTitle("G");
 gG2f->Draw("AP");
+
+gG2f->Fit(funz1,"RM+");
+cout << endl << "tried: [0]*exp(-[1]*x)" << endl;
+cout << "Chi^2:" << funz1->GetChisquare() << ", number of DoF: " << funz1->GetNDF() << " (Probability: " << funz1->GetProb() << ")." << endl;
+float W2= funz1->GetChisquare()/funz1->GetNDF();
+/*gG1f->Fit(funz2,"RM+");
+cout << endl << "tried: [0]*x+[1]" << endl;
+cout << "Chi^2:" << funz2->GetChisquare() << ", number of DoF: " << funz2->GetNDF() << " (Probability: " << funz2->GetProb() << ")." << endl;
+
+*/
+
+//test fisher
+float t_c=9.27;
+cout << 1/t_c << " < " << W1/W2 << " < " << t_c << " ? " << " I due andamenti sono compatibili " << endl;
 }
 
